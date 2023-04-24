fe7703576365c96b19df553ad968b37ea42025d8
Sandip Baikare <baikare.sandeep007@gmail.com>
2023-04-24 14:52:13 +0530

    Updated Page heading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Home - India's Premier EV Charging Station Directory ",
-  description: 'eChargingStations.com is a portal dedicated to providing information on electric vehicle charging stations across India. This platform aims to make it easier for electric vehicle owners to find charging stations near their location, enabling them to travel more conveniently and with peace of mind. Users can search for charging stations based on their location and filter results based on various criteria such as charging speed, availability, and more. In addition to providing information on charging stations, eChargingStations.com also offers resources and articles on electric vehicles and the latest trends in the industry.',
+  title: "Home - Comming soon Template",
+  description: 'This is a simple coming soon template built with NextJS and TailwindCSS. It is a lightweight and responsive template that can be used for various projects that require a "coming soon" page.',
 }
 
 export default function RootLayout({
