24a49ccc79ae27664be88f66a0300d011da4b02b
Sandip Baikare <baikare.sandeep007@gmail.com>
2023-05-02 21:08:06 +0530

    Data file added
    Data.ts file added and enhancement in the page and readme

--- README.md
+++ README.md
@@ -26,6 +26,12 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
+## Future Enhanment
+
+- [x] Comming Soon Template
+- [ ] Email Sent functionality and configurations
+- [ ] Both light and dark colors
+
 ## Learn More
 
 To learn more about Next.js and Tailwind CSS, take a look at the following resources:
---
+++ data/data.ts
@@ -0,0 +1,26 @@
+const data = {
+    sitename: "Comming Soon Template",
+    sitetagline: "Comming Soon Template Tagline",
+    sitelogo: "",
+    title: "Comming Soon",
+    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    newsletterheading: "Stay tuned for our live updates!",
+    copyrightText: 'Copyright © 2023 | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>',
+    socialIconsHeading: "Follow Us 📣",
+    socialIcons: [
+        {
+            icon: "facebook",
+            link: "#",
+        },
+        {
+            icon: "twitter",
+            link: "#",
+        },
+        {
+            icon: "linkedIn",
+            link: "#",
+        },
+    ],
+}
+
+export default data;
