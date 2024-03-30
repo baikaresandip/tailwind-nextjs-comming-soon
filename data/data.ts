8e7a842cee282cc1098c7cae89e0d3fa7e540a2a
Sandip Baikare <baikare.sandeep007@gmail.com>
2024-03-30 23:32:49 +0530

    feat: add wWebsite url in link via data file

--- data/data.ts
+++ data/data.ts
@@ -1,12 +1,14 @@
 const data = {
-    sitename: "Coming Soon Template",
-    sitetagline: "Coming Soon Template Tagline",
+    sitename: "Sandip Baikare",
+    sitetagline: "Something great is on the way!",
+    siteurl: "https://github.com/baikaresandip",
     sitelogo: "",
     title: "Coming Soon",
     description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
     copyrightText: 'Copyright © 2023 | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>',
     socialIconsHeading: "Follow Us 📣",
+    hideSubscribeForm: false, // make true to disable subscription form 
     socialIcons: [
         {
             icon: "facebook",
